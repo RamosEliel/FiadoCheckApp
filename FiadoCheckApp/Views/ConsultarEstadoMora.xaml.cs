ca55a6f07f71c7ae6818d9575280d8e35781b8e9
RamosEliel <davidelielberrioramos@gmail.com>
2025-05-30 19:21:34 -0500

    second commit

---
+++ FiadoCheckApp/Views/ConsultarEstadoMora.xaml.cs
@@ -0,0 +1,10 @@
+namespace FiadoCheckApp.Views;
+using FiadoCheckApp.Views;
+
+public partial class ConsultarEstadoMora : ContentPage
+{
+	public ConsultarEstadoMora()
+	{	
+		InitializeComponent();
+	}
+}
